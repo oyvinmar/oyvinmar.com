776cceeab0c3d66ec8044dd3270780be412acada
Øyvind Marthinsen <oyvinmar@gmail.com>
2012-01-24 22:20:01 +0100

    Created a basic single page express.js app. Added Twitter bootstrap for basic styling. Added some content from oyvinmar.com.

---
+++ app.js
@@ -0,0 +1,46 @@
+var PORT = 8080;
+var express = require('express'), app = express.createServer();
+var stylesheets = __dirname + '/public/stylesheets';
+var images = __dirname + '/public/images';
+app.register('.html', require('hbs'));
+
+/*var less;
+express.compiler.compilers.less.compile = function (str, fn) {
+  if (!less) {
+    less = require("less"); }
+
+  console.log("Test");
+  try {
+    less.render(str, { compress : true }, fn);
+    console.log("Test");
+  }
+  catch (err) {
+    fn(err); }
+};*/
+
+app.configure(function() {
+  console.log("Configure defult settings.");
+  app.use(express.compiler({ src:stylesheets, enable: ['less'] }));
+  app.use(express.bodyParser());
+  app.use(app.router);
+});
+
+app.configure('development',function() {
+  console.log("Configure settings for development.");
+  app.use(express.static(stylesheets));
+  app.use(express.static(images));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true}));
+});
+
+app.configure('production', function() {
+  var oneYear = 31557600000;
+  app.use(express.static(stylesheets, {maxAge: oneYear}));
+  app.use(express.static(images, {maxAge: oneYear}));
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res) {
+  res.render('index.html', {layout:true});
+});
+
+app.listen(process.env.PORT || PORT);
