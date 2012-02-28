62c5878b9ee716ebb4a3d586a179c9215a6eeb4b
Øyvind Marthinsen <oyvinmar@gmail.com>
2012-02-28 22:35:30 +0100

    Added fixed bar on scroll

--- public/javascript/init.js
+++ public/javascript/init.js
@@ -112,4 +112,27 @@ var AppController = Backbone.Router.extend({
 $(function() {
   var apt = new AppController();
   window.app = apt;
+
+  var $spy = $('body');
+  $spy.scrollspy($spy.data());
+  // fix sub nav on scroll
+  var $win = $(window), $nav = $('.subnav'),
+  navTop = $('.subnav').length && $('.subnav').offset().top,
+  isFixed = 0;
+
+  processScroll();
+
+  $win.on('scroll', processScroll)
+
+  function processScroll() {
+    var i, scrollTop = $win.scrollTop()
+    if (scrollTop >= navTop && !isFixed) {
+      isFixed = 1
+      $nav.addClass('subnav-fixed')
+    } else if (scrollTop <= navTop && isFixed) {
+      isFixed = 0
+      $nav.removeClass('subnav-fixed')
+    }
+  }
 });
+
