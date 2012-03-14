cbe1278b06bccfb66c4e371e99fce2c0d91e0201
Øyvind Marthinsen <oyvinmar@gmail.com>
2012-03-14 19:00:26 +0100

    Added subnav and fakenav with smooth scrolling transition. Added som simple styling

--- public/javascript/init.js
+++ public/javascript/init.js
@@ -117,8 +117,9 @@ $(function() {
   $spy.scrollspy($spy.data());
   // fix sub nav on scroll
   var $win = $(window), $nav = $('.subnav'),
-  navTop = $('.subnav').length && $('.subnav').offset().top,
-  isFixed = 0;
+  barTop = $('.subnav').length && $('.subnav').offset().top,
+  navOffsetTop = $('#topnav').length && document.getElementById("topnav").offsetTop,
+  isFixed = 0, isHidden = 0;
 
   processScroll();
 
@@ -126,12 +127,20 @@ $(function() {
 
   function processScroll() {
     var i, scrollTop = $win.scrollTop()
-    if (scrollTop >= navTop && !isFixed) {
-      isFixed = 1
-      $nav.addClass('subnav-fixed')
-    } else if (scrollTop <= navTop && isFixed) {
-      isFixed = 0
-      $nav.removeClass('subnav-fixed')
+    if (scrollTop >= barTop && !isFixed) {
+      isFixed = 1;
+      $nav.addClass('subnav-fixed');
+    } else if (scrollTop >= barTop - navOffsetTop && !isHidden) {
+      $('#topnav').hide();
+      $('#links').fadeIn(700);
+      isHidden = 1;
+    } else if (scrollTop <= barTop && isFixed) {
+      isFixed = 0;
+      $nav.removeClass('subnav-fixed');
+    } else if (scrollTop <= barTop - navOffsetTop && isHidden) {
+      $('#topnav').show();
+      $('#links').hide();
+      isHidden = 0;
     }
   }
 });
