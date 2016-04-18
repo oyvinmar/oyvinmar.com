d632d32151355d53458b9d350a52b745f111b2c8
Øyvind Marthinsen <oma@knowit.no>
2016-04-18 20:36:31 +0200

    Remove redux-router reducer

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import lifestream from './lifestreamReducer';
 import uiReducer from './uiReducer';
-import { routerReducer } from 'react-router-redux';
 
 const rootReducer = combineReducers({
   lifestream,
-  routing: routerReducer,
   ui: uiReducer,
 });
 
