ce736f3b037ee325427008b7345ca05b4c924e0b
Øyvind Marthinsen <oma@knowit.no>
2016-04-18 19:09:41 +0200

    Remove react-router

--- src/app/Index.jsx
+++ src/app/Index.jsx
@@ -7,23 +7,14 @@ import './img/index.js';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { syncHistoryWithStore } from 'react-router-redux';
-import { Router, IndexRoute, Route, browserHistory, hashHistory } from 'react-router';
 import configureStore from './config/configureStore';
 import HomePage from './home/HomePage';
 import CVPage from './cv/CVPage';
 const store = configureStore();
-const history = syncHistoryWithStore(hashHistory, store);
 
 render(
   <Provider store={store}>
-     <Router history={history}>
-       <Route path="/">
-        <IndexRoute component={HomePage}/>
-        <Route component={CVPage} path="cv/"/>
-      </Route>
     <HomePage/>
-    </Router>
   </Provider>,
   document.getElementById('react')
 );
