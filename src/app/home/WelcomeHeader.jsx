09745950c1dcf1830c178853392f39485e21645c
Øyvind Marthinsen <oma@knowit.no>
2017-07-15 09:47:20 +0200

    Responsive hero image

--- src/app/home/WelcomeHeader.jsx
+++ src/app/home/WelcomeHeader.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 
+const getSrcSets = imagePath =>
+  [
+    `${imagePath}_1920.jpg 1920w`,
+    `${imagePath}_1740.jpg 1740w`,
+    `${imagePath}_1440.jpg 1440w`,
+    `${imagePath}_1120.jpg 1120w`,
+    `${imagePath}_1000.jpg 1000w`,
+    `${imagePath}_960.jpg 960w`,
+    `${imagePath}_800.jpg 800w`,
+    `${imagePath}_640.jpg 640w`,
+    `${imagePath}_480.jpg 480w`,
+    `${imagePath}_320.jpg 320w`,
+    `${imagePath}_320.jpg 320w`,
+  ].join(', ');
+
 const WelcomeHeader = () =>
   <section className="welcome" id="home">
-    <img className="hero__img" src="/assets/img/hero-image_1920.jpg" alt="Beautiful sunset"/>
+    <img
+      className="hero__img"
+      src="/assets/img/hero-image_1920.jpg"
+      srcSet={getSrcSets("/assets/img/hero-image")}
+      sizes="100vw"
+      alt="Beautiful sunset"/>
     <div className="overlay" />
     <div className="container-fluid text-center">
       <header>
