3658bfa8496f33f6def523c020931741134705f8
Øyvind Marthinsen <oma@knowit.no>
2017-10-26 20:29:08 +0200

    Add menu state machine

--- src/app/home/NavigationBar.jsx
+++ src/app/home/NavigationBar.jsx
@@ -1,65 +1,78 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as actions from '../actions/uiActions';
+import React, { Component } from 'react';
+import { Machine } from 'xstate';
 
-const NavigationBar = ({ expanded, toogleNavBarLinks }) => {
-  const navClassString = `collapse navbar-collapse${expanded ? ' in' : ''}`;
-  return (
-    <nav className="navbar navbar-default" id="nav">
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <button
-            className="navbar-toggle"
-            onClick={toogleNavBarLinks}
-            type="button">
-            <span className="sr-only">Toggle navigation</span>
-            <i className="fa fa-bars" />
-          </button>
-          <a className="navbar-brand" href="/">
-            oyvinmar.com
-          </a>
-        </div>
-        <div className={navClassString}>
-          <ul className="nav navbar-nav">
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#about">About me</a>
-            </li>
-            <li>
-              <a href="#lifestream">Lifestream</a>
-            </li>
-            <li>
-              <a href="#elsewhere">Elsewhere</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
-};
-
-NavigationBar.propTypes = {
-  toogleNavBarLinks: PropTypes.func.isRequired,
-  expanded: PropTypes.bool.isRequired,
-};
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-      toogleNavBarLinks: actions.toogleNavBarLinks,
+const menuMachine = Machine({
+  key: 'menu',
+  initial: 'hidden',
+  states: {
+    hidden: {
+      on: {
+        TOGGLE: 'visible',
+      },
     },
-    dispatch,
-  );
-
-const mapStateToProps = state => ({
-  expanded: state.ui.isNavBarExpanded,
+    visible: {
+      on: {
+        TOGGLE: 'hidden',
+      },
+    },
+  },
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(NavigationBar);
+class NavigationBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { menuState: menuMachine.initial };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { menuState } = this.state;
+    const nextMenuState = menuMachine.transition(menuState, 'TOGGLE').value;
+    this.setState({ menuState: nextMenuState });
+  }
+
+  render() {
+    const { menuState } = this.state;
+    const isVisible = menuState === 'visible';
+    const navClassString = `collapse navbar-collapse${isVisible ? ' in' : ''}`;
+    return (
+      <nav className="navbar navbar-default" id="nav">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button
+              className="navbar-toggle"
+              onClick={this.handleClick}
+              type="button">
+              <span className="sr-only">Toggle navigation</span>
+              <i className="fa fa-bars" />
+            </button>
+            <a className="navbar-brand" href="/">
+              oyvinmar.com
+            </a>
+          </div>
+          <div className={navClassString}>
+            <ul className="nav navbar-nav">
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#about">About me</a>
+              </li>
+              <li>
+                <a href="#lifestream">Lifestream</a>
+              </li>
+              <li>
+                <a href="#elsewhere">Elsewhere</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default NavigationBar;
--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import lifestream from './lifestreamReducer';
-import uiReducer from './uiReducer';
 
 const rootReducer = combineReducers({
   lifestream,
-  ui: uiReducer,
 });
 
 export default rootReducer;
