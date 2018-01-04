1a11c6aae374e0f35eb41d2a07f4d9e94136a738
Øyvind Marthinsen <oma@knowit.no>
2018-01-04 19:40:31 +0100

    Remove unused code

--- src/app/shapes.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const EventShape = PropTypes.shape({
-  id: PropTypes.string.isRequired,
-  serviceName: PropTypes.string.isRequired,
-  serviceUrl: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired,
-  timestamp: PropTypes.instanceOf(Date),
-});
