593d83024d26bb725f530d32eb4095b7df6fb1e2
Øyvind Marthinsen <oma@knowit.no>
2019-05-13 08:43:58 +0000

    fix: drop proxy caching

--- api/proxy.js
+++ api/proxy.js
@@ -1,32 +1,6 @@
 const https = require('https');
 
-const cache = {};
-
-function CacheObject(timestamp, data) {
-  this.timestamp = timestamp;
-  this.data = data;
-  return this;
-}
-
-function cacheUpdate(key, timestamp, data) {
-  cache[key] = new CacheObject(timestamp, data);
-}
-
-function cacheLookup(key) {
-  return cache[key];
-}
-
-function handleCachedResponse(key, res) {
-  const co = cacheLookup(key);
-  if (co && Date.now() - co.timestamp < 1000 * 60 * 15) {
-    res.set('Content-Type', 'application/json');
-    res.send(co.data);
-    return true;
-  }
-  return false;
-}
-
-function handleResponse(response, res, key) {
+function handleResponse(response, res) {
   let data = '';
   response.on('data', chunk => {
     data += chunk;
@@ -34,18 +8,12 @@ function handleResponse(response, res, key) {
 
   response.setEncoding('utf8');
   response.on('end', () => {
-    cacheUpdate(key, Date.now(), data);
     res.set('Content-Type', 'application/json');
     res.status(response.statusCode).send(data);
   });
 }
 
 function oauthProxyResponder(oauth, res, options) {
-  // Check cache
-  if (handleCachedResponse(options.host, res)) {
-    return;
-  }
-
   oauth.get(
     options.host + options.path,
     options.access_token,
@@ -54,7 +22,6 @@ function oauthProxyResponder(oauth, res, options) {
       if (e) {
         console.error(e);
       }
-      cacheUpdate(options.host, Date.now(), data);
       res.set('Content-Type', 'application/json');
       res.status(response.statusCode).send(data);
     },
@@ -62,10 +29,6 @@ function oauthProxyResponder(oauth, res, options) {
 }
 
 function proxyResponder(res, options) {
-  // Check cache
-  if (handleCachedResponse(options.host, res)) {
-    return;
-  }
   https
     .get(options, response => {
       // console.log("Got response: " + response.statusCode);
