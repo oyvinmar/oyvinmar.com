af48f0e5b826477d008836f6d2839d458f39ff42
Øyvind Marthinsen <EMAIL>
2019-12-28 19:22:41 +0100

    Run format

--- api/endpoints/swarm.js
+++ api/endpoints/swarm.js
@@ -4,9 +4,7 @@ module.exports = (req, res) => {
   const options = {
     host: 'api.foursquare.com',
     port: 443,
-    path: `/v2/users/self/checkins?oauth_token=${
-      process.env.FOURSQUARE_TOKEN
-    }&m=swarm&v=20141808`,
+    path: `/v2/users/self/checkins?oauth_token=${process.env.FOURSQUARE_TOKEN}&m=swarm&v=20141808`,
   };
   proxyResponder(res, options);
 };
