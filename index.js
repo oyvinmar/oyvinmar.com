2717d571ce27cc427f5a8c39f713e65a183915dc
Øyvind Marthinsen <oyvinmar@gmail.com>
2020-12-27 19:59:19 +0100

    bump to tailwind v2

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const createServer = require('./api/serve');
 const file = 'index.html'; // Pass an absolute path to the entrypoint here
 const options = {
   hmrPort: 12345,
+  cache: false,
 }; // See options section of api docs, for the possibilities
 
 // Initialize a new bundler using a file and options
