1191e065d87e24366103e8f2e571def0cd439139
Øyvind Marthinsen <oyvinmar@gmail.com>
2021-07-31 20:30:51 +0200

    re-add rewrite

--- api/polyline.js
+++ api/polyline.js
@@ -3,7 +3,6 @@ const httpProxy = require('http-proxy');
 var proxy = httpProxy.createProxyServer({});
 
 module.exports = async (req, res) => {
-  console.log(req.url);
   req.url =
     req.url.replace(/^\/api\/polyline\//, '') +
     `&key=${process.env.GOOGLE_MAPS_API_KEY}`;
